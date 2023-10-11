17949a98ffc53fd586f19462e2eea4a5f4a8b69f
aksaraicu <fachrul.hasan@gmail.com>
2023-10-11 17:57:36 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const api = {
+  key: "61b5a9dff9b7b348c1b3642d4ac5aff4",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try {
+        const response = await fetch(`${api.base}weather?q=Jakarta&units=imperial&appid=${api.key}`);
+        const data = await response.json();
+        setWeather(data);
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+        setWeather(null); // Set weather data to null in case of an error
+      }
+    };
+
+    fetchWeatherData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Weather App Plaza Indonesia</h1>
+
+        {weather ? (
+          <div>
+            <p>{weather.main.temp}° F</p>
+            <p>{weather.weather[0].description}</p>
+          </div>
+        ) : (
+          <p>Loading weather data...</p>
+        )}
       </header>
     </div>
   );
